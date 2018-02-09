f07654bf619d9875062748b9431407af89d88c27
dwook <igerapex@gmail.com>
2018-02-09 17:56:48 +0900

    seconde commit

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+window.addEventListener("load", ()=> {
+    goIntroPage();
+    }
+)
+
+
 const questionList = [
     { question:'질문에 따른 나이를 입력해 주세요'},
-    { question:'신체활동량을 평가 해 주세요', min:'낮음', max:'높음', choice:3},
+    { question:'신체활동량을 평가 해 주세요', min:'낮음', max:'높음', choice:4},
     { question:'지난 한 달 간, 당신의 전반적인 수면상태를 어떻게 평가하시겠습니까?', min:'나쁨', max:'좋음', choice:4},
     { question:'불안하거나 짜증이 난다', min:'아니다', max:'그렇다', choice:4},
     { question:'자주 피곤하고 쉽게 지친다', min:'아니다', max:'그렇다', choice:8},
@@ -12,54 +18,84 @@ const questionList = [
 let answerList = [];
 
 const boxTest = document.querySelector(".box_test");
-const indexQuestion = document.getElementsByClassName("index_quesiton")[0];
-const boxInput = document.querySelector(".box_input");
-const titQuestion = boxTest.querySelector(".tit_question");
-const listChoice = document.getElementsByClassName("list_choice")[0];
+const boxIntro = document.getElementsByClassName("box_intro")[0];
+const boxQuestion = document.getElementsByClassName("box_question")[0];
+const boxAge = document.getElementsByClassName("box_age")[0];
+const boxInput = document.getElementsByClassName("box_input")[0];
+const boxResult = document.getElementsByClassName("box_result")[0];
+const indexQuestion = document.querySelector(".index_question");
+const titQuestion = document.querySelector(".tit_question");
+const listChoice = document.querySelector(".list_choice");
 const btnChoice = document.getElementsByClassName("btn_choice");
 // const listChoice = document.querySelector(".list_choice");
 // const btnChoice = document.querySelectorAll(".list_choice button");
 const txtMin = document.querySelector(".min");
 const txtMax = document.querySelector(".max");
-const btnStart = document.querySelector(".btn_start");
+const btnStart = document.querySelectorAll(".btn_start");
 const btnPrev = document.querySelector(".btn_prev");
 const btnNext = document.querySelector(".btn_next");
-const scoreTotal = boxTest.querySelector(".scoreTotal");
-const quiz = {
+const scoreTotal = document.querySelector(".score_total");
+const name = document.querySelectorAll(".name");
+const valAge = document.querySelector("#age");
+const valChildbirthAge = document.querySelector("#childbirthAge");
+let msg = document.querySelectorAll(".msg");
+const test = {
+    name: "",
+    age: 0,
+    childbirthAge: 0,
     index: 0,
     score: 0
 }
 
-
-btnStart.addEventListener("click", startQuestion);
+btnStart.forEach( el => el.addEventListener("click", startQuestion));
 btnPrev.addEventListener("click", updateQuestion);
 btnNext.addEventListener("click", updateQuestion);
+valAge.addEventListener("keyup", checkNumber);
+valChildbirthAge.addEventListener("keyup", checkNumber);
+msg.forEach( el => el.addEventListener("transitionend", () => { el.style.opacity = '1'; el.innerHTML ="";}));
 
 
 
 function startQuestion() {
-    quiz.index = 0;
+    test.index = 0;
     answerList = [];
+    boxResult.classList.remove("on");
     updateQuestion();
-    goFirstPage()
+    test.name = prompt("성함이 어떻게 되십니까?")
 }
 
 
 function updateQuestion() {
-    if (quiz.index === questionList.length) { showResult(); return false;}
+  
+    if (this.className === "btn_next") {test.index++;}
+    if (this.className === "btn_prev") {test.index--;}
     
-    if (this.className === "btn_next") {quiz.index++;updateQuestion();}
-    if (this.className === "btn_prev") {quiz.index--;updateQuestion();}
-
-    index = quiz.index;
-    console.dir(this)
+    boxQuestion.classList.add("on")
+    if (test.index === -1) {
+        // 인트로 페이지 이동
+        goIntroPage(); return false;
+    } else if (test.index === 0) { 
+        // 나이 질문 페이지 이동
+        boxIntro.classList.remove("on")
+        boxAge.classList.add("on")
+    } else if (test.index === questionList.length){
+        // 결과 페이지 이동
+        goResultPage(); return false;
+    } else { 
+        boxAge.classList.remove("on");
+        boxInput.classList.add("on"); 
+    }
+    
+    index = test.index;
     currentQuestion = questionList[index];
     indexQuestion.innerText = `Question 0${(index+1)}`;
     titQuestion.innerHTML = currentQuestion.question;
     txtMin.innerHTML = currentQuestion.min || "";
     txtMax.innerHTML = currentQuestion.max || "";
+    test.age = valAge.value;
+    test.childbirthAge = valChildbirthAge.value; 
     const choices = []
-    for (let i = 0; i < currentQuestion.choice ; i++) {
+    for (let i = (index === 1 ?  1 : 0); i < currentQuestion.choice ; i++) {
         choices.push(`<button class="btn_choice" value="${i}" >${i}</button>`) 
     }
     listChoice.innerHTML = choices.join(" ");
@@ -69,12 +105,32 @@ function updateQuestion() {
         btnChoice[i].addEventListener("click", selectChoice);
     }
     
+
+    
     console.log(listChoice);
-    /*
-    btnChoice.forEach( el => el.addEventListener("click", selectChoice));
-    */
+    
+    // btnChoice.forEach( el => el.addEventListener("click", selectChoice));
+    
   
 }
+
+function checkNumber(event){
+    
+    event.stopPropagation()
+    msg = this.nextElementSibling.nextElementSibling;
+    msg.style.transition = 'opacity 1s';
+   
+    
+    if(event.keyCode != 8 && event.keyCode != 0 && (event.keyCode < 48 || event.keyCode > 57)){
+        msg.innerHTML = "숫자만 입력해주세요!";
+        msg.style.opacity = '0';
+        console.dir(msg)
+
+    }
+     
+
+}
+
 
 
 function selectChoice(){
@@ -89,25 +145,30 @@ function showResult(){
 
     const sum = answerList.reduce((a, b) => parseInt(a) + parseInt(b), 0);
     console.log("the total is", sum)
+    return test.score = sum
     
 }
 
-function goFirstPage() {
-    boxInput.innerHTML = `
-        <p>질문에 따른 나이를 입력해 주세요(만 나이로 입력)</p>
-        <p><input type="text" name="" id="age"><span>세</span></p>
-        <p>첫 출산 연령을 입력해 주세요(출산 경험이 없는 경우 00을 입력)<p>
-        <p><input type="text" name="" id="childbirthAge"><span>세</span></p> 
-    `
+
+function goIntroPage() {
+    boxQuestion.classList.remove("on")
+    boxIntro.classList.add("on")
 }
 
-function goLastPage() {
 
-    
+function goResultPage() {
+
+    showResult();
+    boxQuestion.classList.remove("on")
+    boxResult.classList.add("on")
+    name.forEach( el => {el.innerText = test.name});
+    scoreTotal.innerText = test.score;
+  
 }
 
 
 // btnStart.forEach( el => el.addEventListener("click", startQuestion));
 // btnPrev.forEach( el => el.addEventListener("click", updateQuestion(-1)));
 // btnNext.forEach( el => el.addEventListener("click", updateQuestion(+1)));
+
 
